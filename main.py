de6e94f5a0f3feb0f09bb4782d3fbc43087c00fd
Krake24 <krake24@mail.de>
2023-08-07 17:29:33 +0200

    added option to add minutes

--- main.py
+++ main.py
@@ -47,12 +47,15 @@ class Print(disnake.ui.Button):
         await inter.response.send_message(message, ephemeral=True)
 
 class TimerView(disnake.ui.View):
-    def __init__(self):
+    def __init__(self, round_to_hour: bool = True):
         super().__init__()
         self.add_item(timer.TimeChange(addDay, "+1D", disnake.ButtonStyle.danger))
         self.add_item(timer.TimeChange(subtractDay, "-1D", disnake.ButtonStyle.danger))
         self.add_item(timer.TimeChange(addHour, "+1H", disnake.ButtonStyle.primary))
         self.add_item(timer.TimeChange(subtractHour, "-1H", disnake.ButtonStyle.primary))
+        if not round_to_hour:
+            self.add_item(timer.TimeChange(addMinute, "+1M", disnake.ButtonStyle.secondary))
+            self.add_item(timer.TimeChange(subtractMinute, "-1M", disnake.ButtonStyle.secondary))
         self.add_item(Print())
 
 bot = commands.InteractionBot()
@@ -61,20 +64,21 @@ bot = commands.InteractionBot()
 async def timestamp(inter: disnake.AppCmdInter):
     pass
 
-@timestamp.sub_command()
-async def now(inter: disnake.AppCmdInter):
-    t = int(time.time()) - (int(time.time()) % 3600) + 3600
-    await inter.response.send_message(f"<t:{t}>", view=TimerView(), ephemeral=True)
+@timestamp.sub_command(description="Set timestamp based on the current time")
+async def now(inter: disnake.AppCmdInter, round_to_hour: bool = True):
+    t = int(time.time())
+    if round_to_hour:
+        t = t - (int(time.time()) % 3600) + 3600
+    await inter.response.send_message(f"<t:{t}>", view=TimerView(round_to_hour), ephemeral=True)
 
-@timestamp.sub_command()
-async def in_(inter: disnake.AppCmdInter, days: int = 0, hours: int = 0, minutes: int = 0, round: bool = True):
+@timestamp.sub_command(description="Set timestamp based on time from now on")
+async def in_(inter: disnake.AppCmdInter, days: int = 0, hours: int = 0, minutes: int = 0, round_to_hour: bool = True):
     t = time.time()
-    if round:
-        t = int(time.time()) - (int(time.time()) % 3600) + 3600
-    t = int(t) + (days * day) + (hours * hour)
-    if not round:
-        t += minutes * minute
-    await inter.response.send_message(f"<t:{t}>", view=TimerView(), ephemeral=True)
+    if round_to_hour:
+        t = t - (int(time.time()) % 3600) + 3600
+        minutes = 0
+    t = int(t) + (days * day) + (hours * hour) + (minutes * minute)
+    await inter.response.send_message(f"<t:{t}>", view=TimerView(round_to_hour), ephemeral=True)
 
 @bot.event
 async def on_ready():
